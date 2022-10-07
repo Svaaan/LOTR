2a31eae15797c9349967767adc6a7e234c8207b2
Daniel Svan <daniel_svan1@hotmail.com>
2022-10-07 15:14:50 +0200

    Skapar spells

--- Wizard.cs
+++ Wizard.cs
@@ -8,14 +8,27 @@ class Wizard
 
     static void Radagast()
     {
+        //Healing = Heals or resurrect
+        //SummonOftheforest = Summons wild animals that attacks
+        //Reshape/Hex = reshapes opponents to a random animal
+        //Eagle = summons eagle with x amount health that takes order from radagast
+        //Ultimate(resurrect) = brings back the dying to life
 
     }
     static void Gandalf()
     {
+        //Light = Blinds the oppenent and affect it with fear
+        //Fire = Cast fire on opponent
+        //Compulsion = Taunt the enemy aswell as doing it weak
+        //Telekinesis = Move items (suprise attack)
+        //Ultimate(Flash) = Dealing damage to enemies within an certain area.
 
     }
     static void Saruman()
     {
+        //Urukhai = Spawn 2 urukhai to fight in his place
+        //Speech = Takes control of the enemies and cast his own spells against him
+        //
 
     }
     static void Sauron()
