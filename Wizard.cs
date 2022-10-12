d670dd18f23f0702b300bc7fc51881de06746ce4
Daniel Svan <daniel_svan1@hotmail.com>
2022-10-12 12:47:11 +0200

    ladde in mer attribut till wizards

--- Wizard.cs
+++ Wizard.cs
@@ -8,34 +8,36 @@ class Wizard : Character
         wizRadagast.name = "Radagast";
         wizRadagast.age = "Since the first tree";
         wizRadagast.type = "Wizard of the forest";
-        wizRadagast.spells = " Healing = Heals or resurrect + SummonOftheforest = Summons wild animals that attacks n\ Reshape/Hex = reshapes opponents to a random animal + Eagle = summons eagle with x amount health that takes order from radagast + Ultimate(resurrect) = brings back the dying to life";
+        wizRadagast.spells = " Healing = Heals or resurrect + SummonOftheforest = Summons wild animals that attacks + Reshape/Hex = reshapes opponents to a random animal + Eagle = summons eagle with x amount health that takes order from radagast + Ultimate(resurrect) = brings back the dying to life";
+        wizRadagast.info = "Lover of the forest and animal friend. Radagast has the nature within his fist";
         
        
         
 
     }
-    static void Gandalf()
+    static void GandalfTheGray()
     {
         Wizard wizGandalf = new Wizard ();
-        //Light = Blinds the oppenent and affect it with fear
-        //Fire = Cast fire on opponent
-        //Compulsion = Taunt the enemy aswell as doing it weak
-        //Telekinesis = Move items (suprise attack)
-        //Ultimate(Flash) = Dealing damage to enemies within an certain area.
+
+        wizGandalf.name = "Gandalf (Mithrandir)";
+        wizGandalf.age = "Since the first age and reborned into new ones";
+        wizGandalf.type = "Master off light and fire";
+        wizGandalf.spells = "Light = Blinds the oppenent and affect it with fear + Fire = Cast fire on opponent + Compulsion = Taunt the enemy aswell as doing it weak +Telekinesis = Move items (suprise attack) + Ultimate(Flash) = Dealing damage to enemies within an certain area.";
+        wizGandalf.info = "Saviour off middle earth, wisest of the wizards and wearer of white";
 
     }
-    static void Saruman()
+    static void SarumanTheWhite()
     {
         Wizard wizSaruman = new Wizard ();
 
-        //Urukhai = Spawn 2 urukhai to fight in his place
-        //Ensnare = Stuns enemies 
-        //Explosive = Cause an explosion that inflict damage to the opponent
-        //Boost = Give all allies around Saruman +attack, + speed + armor.
-        //Ultimate(Speech) = Takes control of the enemies to cast their own spells against them self)
-
-    }
-
-
-    
+        wizSaruman.name = "Saruman";
+        wizSaruman.age = "Oldest of the 5";
+        wizSaruman.type = "Deciever and pursvasive, wisest of the 5";
+        wizSaruman.spells = "Urukhai = Spawn 2 urukhai to fight in his place";
+        wizSaruman.spells = "Ensnare = Stuns enemies";
+        wizSaruman.spells = "Explosive = Cause an explosion that inflict damage to the opponent";
+        wizSaruman.spells = "Boost = Give all allies around Saruman +attack, + speed + armor.";
+        wizSaruman.spells = " Ultimate(Speech) = Takes control of the enemies to cast their own spells against them self)";
+        wizSaruman.info = "Lord of Isengard, Heade of the white council.";
+    } 
 }
